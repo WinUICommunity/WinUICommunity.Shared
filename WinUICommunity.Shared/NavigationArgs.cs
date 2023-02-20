771bb2e47e93a4df95309431aa9a9c56f0fcf18a
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-02-20 22:09:24 +0330

    Add some classes

---
+++ WinUICommunity.Shared/NavigationArgs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using WinUICommunity.Shared.DataModel;
+
+namespace WinUICommunity.Shared.Internal;
+
+public class NavigationArgs
+{
+    public NavigationView NavigationView;
+    public object Parameter;
+    public string JsonRelativeFilePath;
+    public IncludedInBuildMode IncludedInBuildMode;
+}
